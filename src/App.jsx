d37648f166a62e10e638adf245287cbda9b5358a
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-05-09 06:58:21 +0300

    Add the home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Login, Signup } from './routes'
+import { Login, Signup, Home } from './routes'
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+		<div>
+			<Router>
+				<Routes>
+				  <Route path="/home" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/routes/home/Home.jsx
@@ -0,0 +1,5 @@
+export function Home(){
+    return (
+        <h1>Hello From Home</h1>
+    )
+}
