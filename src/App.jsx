4b6b5d8e46b34fc54e46c3e661efd4a5eedd9450
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-05-09 06:54:07 +0300

    Add routing for the login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { Login } from './routes'
-import { Signup } from './routes'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Login, Signup } from './routes'
 
 function App() {
   return (
     <div>
-      {/* <Login /> */}
-      <Signup />
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
